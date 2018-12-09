3537aaecb155d106fbfae83c43ddd787bc31fbbf
Sebastian Monsrud <35904092+SebiMonsrud@users.noreply.github.com>
2018-12-09 23:50:43 +0100

    v3.1 alpha
    -added partial suport for healthbar(need more work)

---
+++ TopDownMMORPG/Assets/EnemyManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    GameObject _canvas;
+
+    public GameObject _slider;
+
+    public GameObject[] Enemies;
+    public Slider[] EnemyHealthSliders;
+
+    void Start()
+    {
+        Enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        _canvas = GameObject.Find("Canvas");
+        
+    }
+    
+    void Update()
+    {
+        for (int i = 0; i < EnemyHealthSliders.Length; i++)
+        {
+            EnemyHealthSliders[i].GetComponent<Slider>().value = 100;
+            EnemyHealthSliders[i].transform.position = Enemies[i].transform.position;
+        }
+    }
+}
