802e7f0f1c03c54cacbb78c1ac3fd158405fa1dc
Sebastian Monsrud <35904092+SebiMonsrud@users.noreply.github.com>
2018-12-11 21:31:31 +0100

    v3.3 alpha
    -added bow and arrow (can't shoot yet. needs to be fixed)
    -addded suport for more than 1 weapon/equipment

--- TopDownMMORPG/Assets/Script/PlayerScripts/PlayerEquipment.cs
+++ TopDownMMORPG/Assets/Script/PlayerScripts/PlayerEquipment.cs
@@ -7,9 +7,12 @@ public class PlayerEquipment : MonoBehaviour
 
     //                <>
 
-    public GameObject[] Equipment;
+    public GameObject[] Equipments;
     public GameObject ActiveEquipment;
     public bool ActiveWeapon;
+
+    public GameObject ArrowToSpawn;
+    public GameObject[] Arrows;
 
     public bool IsAttacking;
 
@@ -20,16 +23,45 @@ public class PlayerEquipment : MonoBehaviour
     void Start()
     {
         _SpriteRenderer = GetComponent<SpriteRenderer>();
-        ActiveEquipment = Equipment[0];
+        ActiveEquipment = Equipments[0];
         ActiveWeapon = true;
     }
 
     void Update()
     {
 
+        if (Input.GetKeyDown(KeyCode.Alpha1) && !IsAttacking && ActiveEquipment != Equipments[0])
+        {
+            ActiveEquipment = Equipments[0];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && !IsAttacking && ActiveEquipment != Equipments[1])
+        {
+            ActiveEquipment = Equipments[1];
+        }
+
+        foreach (GameObject equipment in Equipments)
+        {
+            if(equipment != ActiveEquipment)
+            {
+                equipment.SetActive(false);
+            }
+            else
+            {
+                equipment.SetActive(true);
+            }
+        }
+
         if (Input.GetButtonDown("Fire1") && !IsAttacking)
         {
-            StartCoroutine(SwingSwordAnim());
+            if(ActiveEquipment == Equipments[0])
+            {
+                StartCoroutine(SwingSwordAnim());
+            }
+            else if(ActiveEquipment == Equipments[1])
+            {
+                StartCoroutine(DrawBowAnim());
+            }
+
             DamageEnemies();
         }
     }
@@ -65,6 +97,27 @@ public class PlayerEquipment : MonoBehaviour
         }
     }
 
+    IEnumerator DrawBowAnim()
+    {
+        IsAttacking = true;
+        bool a = false;
+        while (!a)
+        {
+            for (int i = 0; i < Arrows.Length || a == true; i++)
+            {
+                if(Arrows[i] == null)
+                {
+                    a = true;
+                }
+            }
+        }
+
+        Instantiate(ArrowToSpawn, this.transform.position, this.transform.rotation);
+
+        yield return new WaitForSeconds(0.5f);
+        IsAttacking = false;
+    }
+
     IEnumerator SwingSwordAnim()
     {
         IsAttacking = true;
@@ -78,38 +131,55 @@ public class PlayerEquipment : MonoBehaviour
 
     void DamageEnemies()
     {
-        foreach (GameObject enemy in EnemiesAroundPlayer)
+        if (ActiveEquipment = Equipments[0])
         {
-            if(enemy != null)
+            foreach (GameObject enemy in EnemiesAroundPlayer)
             {
-                float TimeUntilDamageApply = 0;
-
-                Vector3 vec1 = enemy.transform.position;
-                Vector3 vec2 = this.transform.position;
-                Vector3 vec3 = this.transform.position + new Vector3(10f, 0);
-
-                float lenghtA = Mathf.Sqrt(Mathf.Pow(vec2.x - vec1.x, 2) + Mathf.Pow(vec2.y - vec1.y, 2));
-                float lenghtB = Mathf.Sqrt(Mathf.Pow(vec3.x - vec2.x, 2) + Mathf.Pow(vec3.y - vec2.y, 2));
-                float lenghtC = Mathf.Sqrt(Mathf.Pow(vec3.x - vec1.x, 2) + Mathf.Pow(vec3.y - vec1.y, 2));
-
-                float calc = ((lenghtA * lenghtA) + (lenghtB * lenghtB) - (lenghtC * lenghtC)) / (2 * lenghtA * lenghtB);
-
-                float angle =  Mathf.Acos(calc) * Mathf.Rad2Deg;
-
-                if(enemy.transform.position.y > this.transform.position.y)
+                if (enemy != null)
                 {
-                    angle -= angle * 2f; 
+                    float TimeUntilDamageApply = 0;
+
+                    Vector3 vec1 = enemy.transform.position;
+                    Vector3 vec2 = this.transform.position;
+                    Vector3 vec3 = this.transform.position + new Vector3(10f, 0);
+
+                    float lenghtA = Mathf.Sqrt(Mathf.Pow(vec2.x - vec1.x, 2) + Mathf.Pow(vec2.y - vec1.y, 2));
+                    float lenghtB = Mathf.Sqrt(Mathf.Pow(vec3.x - vec2.x, 2) + Mathf.Pow(vec3.y - vec2.y, 2));
+                    float lenghtC = Mathf.Sqrt(Mathf.Pow(vec3.x - vec1.x, 2) + Mathf.Pow(vec3.y - vec1.y, 2));
+
+                    float calc = ((lenghtA * lenghtA) + (lenghtB * lenghtB) - (lenghtC * lenghtC)) / (2 * lenghtA * lenghtB);
+
+                    float angle = Mathf.Acos(calc) * Mathf.Rad2Deg;
+
+                    if (enemy.transform.position.y > this.transform.position.y)
+                    {
+                        angle -= angle * 2f;
+                    }
+
+                    angle -= 180f;
+
+                    angle = -angle;
+
+                    Debug.Log("Angle = " + angle + " | on Entety: " + enemy.transform.name);
+
+                    TimeUntilDamageApply = (0.0020f * angle);
+
+                    enemy.GetComponent<EnemyScript>().GetHit(this.gameObject, TimeUntilDamageApply);
                 }
-
-                angle -= 180f;
-
-                angle = -angle;
-
-                Debug.Log("Angle = " + angle + " | on Entety: " + enemy.transform.name);
-
-                TimeUntilDamageApply = (0.0020f * angle);
-
-                enemy.GetComponent<EnemyScript>().GetHit(this.gameObject, TimeUntilDamageApply);
+            }
+        }
+        else if (ActiveEquipment == Equipments[1])
+        {
+            for (int i = 0; i < Arrows.Length; i++)
+            {
+                if(Arrows[i].GetComponent<ArrowScript>().Hit != null)
+                {
+                    if (Arrows[i].GetComponent<ArrowScript>().Hit.GetComponent<EnemyScript>() != null)
+                    {
+                        Arrows[i].GetComponent<ArrowScript>().Hit.GetComponent<EnemyScript>().GetHit(this.gameObject, 0.05f);
+                        GameObject.Destroy(Arrows[i]);
+                    }
+                }
             }
         }
     }
