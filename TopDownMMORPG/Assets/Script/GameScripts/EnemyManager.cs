281614b086f4236ada209c6b0df9c151a4674fd9
Sebastian <sebimonsrud@gmail.com>
2020-09-27 03:41:04 +0200

    v4.4.1
    v4.4 but with more :)
    added level system for enemies
    added better healthbar and UI over enemies
    added new enemy (mage) **BUGGED
    Untagled spaghetti code
    adjusted health and damage values for player and enemies

--- TopDownMMORPG/Assets/Script/GameScripts/EnemyManager.cs
+++ TopDownMMORPG/Assets/Script/GameScripts/EnemyManager.cs
@@ -8,10 +8,10 @@ public class EnemyManager : MonoBehaviour
 
     GameObject _canvas;
 
-    public GameObject _slider;
+    public GameObject EnemyUI;
 
     public GameObject[] Enemies;
-    public GameObject[] EnemyHealthSliders;
+    public GameObject[] EnemyUIs;
 
     void Start()
     {
@@ -19,10 +19,10 @@ public class EnemyManager : MonoBehaviour
         _canvas = GameObject.Find("EnemyCanvas");
         for (int i = 0; i < Enemies.Length; i++)
         {
-            if(EnemyHealthSliders[i] == null)
+            if(EnemyUIs[i] == null)
             {
-                GameObject CurrentEnemySlider = Instantiate(_slider, _canvas.transform);
-                EnemyHealthSliders[i] = CurrentEnemySlider;
+                GameObject CurrentEnemyUI = Instantiate(EnemyUI, _canvas.transform);
+                EnemyUIs[i] = CurrentEnemyUI;
             }
         }
         
@@ -30,20 +30,20 @@ public class EnemyManager : MonoBehaviour
     
     void Update()
     {
-        for (int i = 0; i < EnemyHealthSliders.Length && i < Enemies.Length; i++)
+        for (int i = 0; i < EnemyUIs.Length && i < Enemies.Length; i++)
         {
-            if(Enemies[i] != null && EnemyHealthSliders[i] != null)
+            if(Enemies[i] != null && EnemyUIs[i] != null)
             {
-                EnemyHealthSliders[i].transform.position = Enemies[i].transform.position + new Vector3(0, Enemies[i].GetComponent<CircleCollider2D>().bounds.extents.y);
-                Enemies[i].GetComponent<EnemyScript>()._HealthSlider = EnemyHealthSliders[i];
+                EnemyUIs[i].transform.position = Enemies[i].transform.position + new Vector3(0, Enemies[i].GetComponent<CircleCollider2D>().bounds.extents.y);
+                Enemies[i].GetComponent<EnemyScript>()._enemyUI = EnemyUIs[i];
 
-            }else if(EnemyHealthSliders[i] == null && Enemies[i] != null)
+            }else if(EnemyUIs[i] == null && Enemies[i] != null)
             {
-                GameObject CurrentEnemySlider = Instantiate(_slider, _canvas.transform);
-                EnemyHealthSliders[i] = CurrentEnemySlider;
+                GameObject CurrentEnemyUI = Instantiate(EnemyUI, _canvas.transform);
+                EnemyUIs[i] = CurrentEnemyUI;
                 
-                EnemyHealthSliders[i].transform.position = Enemies[i].transform.position + new Vector3(0, Enemies[i].GetComponent<CircleCollider2D>().bounds.extents.y);
-            }else if(Enemies[i] == null && EnemyHealthSliders[i] == null)
+                EnemyUIs[i].transform.position = Enemies[i].transform.position + new Vector3(0, Enemies[i].GetComponent<CircleCollider2D>().bounds.extents.y);
+            }else if(Enemies[i] == null && EnemyUIs[i] == null)
             {
             }
         }
